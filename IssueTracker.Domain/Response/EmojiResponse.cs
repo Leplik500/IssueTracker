8eeaf1efe2fdbb3ca829e75976f4ecf571aafe94
naotomo <nikitin-ao-dev@yandex.ru>
2024-06-12 00:21:41 +0300

    feat: fetching emoji by :shortcode: from api

---
+++ IssueTracker.Domain/Response/EmojiResponse.cs
@@ -0,0 +1,10 @@
+namespace IssueTracker.Domain.Response;
+
+public class EmojiResponse {
+    public String code { get; set; }
+    public String character { get; set; }
+    public String image { get; set; }
+    public String name { get; set; }
+    public String group { get; set; }
+    public String subgroup { get; set; }
+}
